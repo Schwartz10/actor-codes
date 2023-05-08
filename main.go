f2cbe0d106f0c2cd6386a063506ade404a59f31f
Jonathan <jpschwartz2@uwalumni.com>
2023-05-08 16:49:50 -0700

    Replication repo

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-jsonrpc"
+	actorstypes "github.com/filecoin-project/go-state-types/actors"
+	"github.com/filecoin-project/go-state-types/manifest"
+	lotusapi "github.com/filecoin-project/lotus/api"
+	"github.com/filecoin-project/lotus/chain/actors"
+	"github.com/filecoin-project/lotus/chain/types"
+)
+
+func main() {
+	var api lotusapi.FullNodeStruct
+	head := http.Header{}
+
+	closer, err := jsonrpc.NewMergeClient(
+		context.Background(),
+		"https://api.calibration.node.glif.io/rpc/v1",
+		"Filecoin",
+		lotusapi.GetInternalStructs(&api),
+		head,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	defer closer()
+
+	minerAddr, err := address.NewFromString("f01001")
+
+	actor, err := api.StateGetActor(context.Background(), minerAddr, types.TipSetKey{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Miner actor code from StateGetActor: %s\n", actor.Code.String())
+
+	actorCode, success := actors.GetActorCodeID(actorstypes.Version(actors.LatestVersion), manifest.MinerKey)
+	if !success {
+		panic("failed to get actor codes")
+	}
+
+	fmt.Printf("Miner actor code from GetActorCodeID: %s\n", actorCode)
+
+}
